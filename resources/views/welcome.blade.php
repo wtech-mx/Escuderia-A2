9c404b9ed8b060aaf61ac1373d7bd2e5e4b8995e
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-01-27 18:21:29 -0600

    update log responsividad

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,8 +9,7 @@
 
             <div class="container-fluid">
 
-                <div class="row  bg-img-log" style="background-image: url({{ asset('img/bg-log.png') }});">
-
+                <div class="row bg-img-log" style="background-image: url({{ asset('img/bg-log.png') }});">
                     <div class="col-md-12 p-5">
 
                         <h1 class="text-left mb-3 titulo-welcome">
@@ -21,38 +20,34 @@
                             EAGO TU SOLUCION
                         </h5>
 
-                        <div class="d-flex justify-content-center" style="margin-top: 6rem !important;">
+                        <div class="d-flex justify-content-center mt-5" >
                             <img class="img-responsive" width="45%" src="https://eago.com.mx/general/1595621309.png" />
                         </div>
 
                     </div>
 
-                    <svg  preserveAspectRatio="none" viewBox="0 0 1900 95" class="d-block" width="180%" height="150px">
-                        <path d="m1800 0v50h-1800z" fill="#050f55"></path>
-                    </svg>
+                    <div class="divider" style="position: absolute;top: 78%">
+                        <svg  preserveAspectRatio="none" viewBox="0 0 1900 125" class="d-block" width="150%" height="200px" style="z-index: 1000;position: relative">
+                         <path d="m1800 0v50h-1800z" fill="#050f55"></path>
+                        </svg>
+                    </div>
 
                 </div>
 
-
-
-                <div class="row bg-down-blue">
-
-                    <div class="col-md-12 text-center p-5">
-
-                        <div class="d-flex justify-content-center mb-2">
-                            <a class="d-block btn  btn-lg  btn-perzonalizado-gradient text-white" href="{{ route('login') }}">
+                 <div class="row bg-down-blue">
+                    <div class="col-12 mt-5">
+                        <div class="container-login100-form-btn">
+                            <a class="btn btn-lg  btn-perzonalizado-gradient text-white mb-3" href="{{ route('login') }}">
                                 {{ __('Login') }}
                             </a>
-                        </div>
 
-                        <div class="d-flex justify-content-center ">
-                            <a class="d-block btn  btn-lg btn-perzonalizado-white text-dark" href="{{ route('register') }}">
+                            <a class=" btn btn-lg btn-perzonalizado-white text-dark mb-5" href="{{ route('register') }}">
                                 {{ __('Register') }}
                             </a>
                         </div>
-
                     </div>
                 </div>
+
             </div>
 
 		</div>
