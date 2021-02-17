0ed9ecd612e132f2b769ed54b12830ddb570f6c8
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-02-16 20:58:12 -0600

    Add controllers

--- app/Http/Controllers/ExpreemplacaminetoController.php
+++ app/Http/Controllers/ExpreemplacaminetoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ExpedientesFisicosController extends Controller
+class ExpreemplacaminetoController extends Controller
 {
     //
 }
