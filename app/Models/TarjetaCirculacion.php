75a4f2debf82539e143343bd95cfd9d4697d8378
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-02-26 14:47:12 -0600

    Revert "Update colros"
    This reverts commit a353976c861a4a778cee2152df1e5eb2407140f4.

--- app/Models/TarjetaCirculacion.php
+++ app/Models/TarjetaCirculacion.php
@@ -8,32 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class TarjetaCirculacion extends Model
 {
     use HasFactory;
-
-    protected $table = "tarjeta_circulacion";
-    protected $primarykey = "id";
-
-    protected $fillable = [
-        'id_user',
-        'id_empresa',
-        'current_auto',
-        'nombre',
-        'tipo_placa',
-        'lugar_expedicion',
-        'fecha_emision',
-    ];
-
-    protected $guarded=[
-
-    ];
-
-    public function User()
-    {
-       return $this->belongsTo(User::class,'id_user');
-    }
-
-        public function Automovil()
-    {
-       return $this->belongsTo(Automovil::class,'current_auto');
-    }
-
 }
