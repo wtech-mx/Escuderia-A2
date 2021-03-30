6498d98ed0c2a2a5f6c44e8beaf9019e0b14b454
verplancken <42614785+verplancken@users.noreply.github.com>
2021-03-30 13:40:41 -0600

    se quito el boton

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -44,17 +44,14 @@
                         </h5>
                     </div>
 
-                        <div class="col-2 mt-5">
-                            <div class="d-flex justify-content-start">
-                                <div class="text-center text-white bg-white" style="border-radius: 50px;padding: 5px">
-                                  <img class="" src="{{ asset('img/icon/color/campana.png') }}" width="30px" >
-                                </div>
-                            </div>
+                    <div class="col-2">
+                        <div class="d-flex justify-content-start">
+                                    <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()"  class="text-center text-white bg-white" style="border-radius: 50px;padding: 5px">
+                                      <img class="" src="{{ asset('img/icon/color/campana.png') }}" width="30px" >
+                                    </button>
                         </div>
-
-                                            <div class="col-12">
-                         <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
                     </div>
+
                     <style>
                         #calendar{
                             margin: 0px auto;
