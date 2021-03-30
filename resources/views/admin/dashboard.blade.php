a7b0ae9375b6209b6ab9c93a0fa45d60aa95909b
verplancken <42614785+verplancken@users.noreply.github.com>
2021-03-30 13:38:40 -0600

    botno alertas

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -51,11 +51,15 @@
 
                         <div class="col-2 mt-5">
                             <div class="d-flex justify-content-start">
-                                    <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()"  class="text-center text-white bg-white" style="border-radius: 50px;padding: 5px">
-                                      <img class="" src="{{ asset('img/icon/color/campana.png') }}" width="30px" >
-                                    </button>
+                                <div class="text-center text-white bg-white" style="border-radius: 50px;padding: 5px">
+                                  <img class="" src="{{ asset('img/icon/color/campana.png') }}" width="30px" >
+                                </div>
                             </div>
                         </div>
+
+                                            <div class="col-12">
+                         <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()" class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
+                    </div>
 
                         <div class="col-12 p-3 ">
                             <div class=" d-flex justify-content-between bg-white p-2 rounded-pill">
