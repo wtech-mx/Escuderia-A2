30959068250d0cc243d1ab7a859cfa71fb22e873
GOLDBN <52049354+GOLDBN@users.noreply.github.com>
2021-04-10 02:11:19 -0500

    update login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,25 +4,25 @@
 
 
 
-    <div class="row bg-img-log" style="background-image: url({{ asset('img/bg-log.png') }});height: 50vh !important; overflow: hidden!important;">
+    <div class="row bg-down-blue" style="border-radius: 0px;background: #191E25 0% 0% no-repeat padding-box;height: 50vh !important; overflow: hidden!important;">
 
-        <div class="col-md-12 mt-3">
+        <div class="col-md-12" style="margin-top: 160px">
 
-            <h1 class="text-left mb-3 titulo-welcome">
-                Bienvenido
-            </h1>
+{{--            <h1 class="text-left mb-5 titulo-welcome">--}}
+{{--                Bienvenido--}}
+{{--            </h1>--}}
 
             <div class="d-flex justify-content-center mt-5" >
-                <img  class="img-responsive" width="65%" src="{{ asset('img/logo-check.png') }}" alt="logo check go">
+                <img  class="img-responsive" width="75%" src="{{ asset('img/logo-check.png') }}" alt="logo check go">
             </div>
 
         </div>
 
-        <div class="divider" style="position: absolute;top: 63%">
-            <svg  preserveAspectRatio="none" viewBox="0 0 1900 125" class="d-block" width="150%" height="52vh" style="z-index: 1000;position: relative">
-                <path d="m1800 0v50h-1800z" fill="#191E25"></path>
-            </svg>
-        </div>
+{{--        <div class="divider" style="position: absolute;top: 63%">--}}
+{{--            <svg  preserveAspectRatio="none" viewBox="0 0 1900 125" class="d-block" width="150%" height="52vh" style="z-index: 1000;position: relative">--}}
+{{--                <path d="m1800 0v50h-1800z" fill="#191E25"></path>--}}
+{{--            </svg>--}}
+{{--        </div>--}}
 
     </div>
 
