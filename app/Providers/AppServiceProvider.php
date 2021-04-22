69ed45dccca0924deefd9c282bff809b7d32744e
verplancken <42614785+verplancken@users.noreply.github.com>
2021-04-21 23:15:08 -0500

    One Signal
    Envío de alerta de seguro el mismo día

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -9,6 +9,7 @@ use App\Models\Seguros;
 use App\Models\TarjetaCirculacion;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use OneSignal;
 
 
 class AppServiceProvider extends ServiceProvider
@@ -35,7 +36,7 @@ class AppServiceProvider extends ServiceProvider
 
                   view()->composer('admin.layouts.app', function($view){
                             // obtener la hora actual  - 2015-12-19 10:10:54
-                      $current = Carbon::now()->toDateTimeString();
+                      $current = Carbon::now()->toDateString();
                       $alert2 = Alertas::
                         where('id_user', '=', auth()->user()->id)
                         ->where('estatus', '=', 0)
@@ -44,10 +45,8 @@ class AppServiceProvider extends ServiceProvider
 
                       //Trae la alerta Seguro
                       $seguro_alerta = Seguros::
-                        where('id_user', '=', auth()->user()->id)
-                        ->where('estatus', '=', 0)
-                        ->where('end','<=', $current)
-                        ->get();
+                        where('end','=', $current)
+                        ->first();
 
                       //Trae la alerta Tc
                       $tc_alerta = TarjetaCirculacion::
@@ -63,12 +62,33 @@ class AppServiceProvider extends ServiceProvider
                         ->where('end','<=', $current)
                         ->get();
 
+                      if($seguro_alerta != NULL){
+                          if ($seguro_alerta->end == $current){
+                          //    Inicio Alerta
+                                $fecha = $seguro_alerta->end.' 23:10 '.'GMT-5';
+
+                                $params = [];
+                                $params['include_player_ids'] = [$seguro_alerta->device_token];
+                                $contents = [
+                                   "en" => $seguro_alerta->descripcion
+                                ];
+                                $params['contents'] = $contents;
+                                $params['delayed_option'] = "timezone"; // Will deliver on user's timezone
+                                $params['send_after'] = $fecha; // Delivery time
+
+                                OneSignal::sendNotificationCustom($params);
+                          //    Fin Alerta
+                          }
+
+                             $seguro_alerta->estatus = 1;
+                             $seguro_alerta->save();
+                      }
                       $view->with(['alert2'=> $alert2, 'seguro_alerta'=> $seguro_alerta, 'tc_alerta'=> $tc_alerta, 'verificacion'=> $verificacion]);
                   });
 
                   view()->composer('layouts.app', function($view2){
                       // obtener la hora actual  - 2015-12-19 10:10:54
-                      $current = Carbon::now()->toDateTimeString();
+                      $current = Carbon::now()->toDateString();
                       $alert2 = Alertas::
                         where('id_user', '=', auth()->user()->id)
                         ->where('estatus', '=', 0)
@@ -78,9 +98,10 @@ class AppServiceProvider extends ServiceProvider
                       //Trae la alerta Seguro
                       $seguro_alerta = Seguros::
                         where('id_user', '=', auth()->user()->id)
+                        ->where('current_auto', '=', auth()->user()->current_auto)
                         ->where('estatus', '=', 0)
                         ->where('end','<=', $current)
-                        ->get();
+                        ->first();
 
                       //Trae la alerta Tc
                       $tc_alerta = TarjetaCirculacion::
@@ -124,26 +145,27 @@ class AppServiceProvider extends ServiceProvider
                         ->where('end','<=', $current)
                         ->first();
 
+                      if($seguro_alerta2 != NULL){
+                          if ($seguro_alerta->end == $current){
+                          //    Inicio Alerta
+                                $fecha = $seguro_alerta->end.' 23:10 '.'GMT-5';
 
-                        if($alert3 != NULL){
-                            $alert3->estatus = 1;
-                             $alert3->save();
-                         }
+                                $params = [];
+                                $params['include_player_ids'] = [$seguro_alerta->device_token];
+                                $contents = [
+                                   "en" => $seguro_alerta->descripcion
+                                ];
+                                $params['contents'] = $contents;
+                                $params['delayed_option'] = "timezone"; // Will deliver on user's timezone
+                                $params['send_after'] = $fecha; // Delivery time
 
-                        if($seguro_alerta2 != NULL){
+                                OneSignal::sendNotificationCustom($params);
+                          //    Fin Alerta
+                          }
+
                              $seguro_alerta2->estatus = 1;
                              $seguro_alerta2->save();
-                         }
-
-                        if($tc_alerta2 != NULL){
-                             $tc_alerta2->estatus = 1;
-                             $tc_alerta2->save();
-                         }
-
-                        if($verificacion2 != NULL){
-                             $verificacion2->estatus = 1;
-                             $verificacion2->save();
-                         }
+                      }
 
                       $view2->with(['alert2'=> $alert2, 'seguro_alerta'=> $seguro_alerta, 'tc_alerta'=> $tc_alerta, 'verificacion'=> $verificacion]);
                   });
