eead49399698f3b65521a0a5a3e8c0b803d48e47
verplancken <42614785+verplancken@users.noreply.github.com>
2021-07-26 14:41:06 -0500

    Correcion de color pronostico

--- app/Http/Controllers/PronosticoController.php
+++ app/Http/Controllers/PronosticoController.php
@@ -49,7 +49,7 @@ class PronosticoController extends Controller
         $alert->descripcion = $request->get('descripcion');
         $alert->start = $request->get('end');
         $alert->end = $request->get('end');
-        $alert->color = '#e7ab1d';
+        $alert->color = '#E0385D';
         $alert->image = $request->get('image');
         $alert->estatus = 0;
         $alert->check = 0;
