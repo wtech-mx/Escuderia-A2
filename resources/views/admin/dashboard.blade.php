6ca9645bfd6c05211d5a4904cf623e27cb3cc86f
verplancken <42614785+verplancken@users.noreply.github.com>
2024-11-04 18:20:31 -0600

    cerrar sesion

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -24,8 +24,8 @@
             <div class="col-xs-12 col-sm-12 col-lg-6 col-xl-6 p-3 ">
 
                 <div class="d-flex justify-content-between mt-3 mb-3">
-                        <a class="btn" data-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false"
-                            aria-controls="multiCollapseExample1">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <div class="text-center text-white bg-white" style="border-radius: 50px;padding: 5px">
                                 @if ($users->img == null)
                                     <i class="fas fa-user" style="color: #000000;width: 40px"></i>
@@ -33,7 +33,17 @@
                                     <img class="rounded-circle" src="{{ asset('img-perfil/' . $users->img) }}" width="40px">
                                 @endif
                             </div>
-                        </a>
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                    Cerrar sesión
+                                </a>
+                        </div>
+                    </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
 
                         <h5 class="text-center text-white ml-4 mr-4">
                             <strong>Hola : {{ $users->name }}</strong> <br> <br>
